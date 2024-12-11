ef2653b3af0f5d94683bb98c01fdc762014b0dad
PhilipCodeNinja <philip@famreiber.de>
2024-12-11 22:41:56 +0000

    2. Triangle on Canvas

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill((0, 0, 0))
-        # pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)).fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(60)/1000
 
 
 
