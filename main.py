9bade781853e95b20618979c3aa84e31d70aba4f
PhilipCodeNinja <philip@famreiber.de>
2024-12-11 23:28:05 +0000

    3. Triangle moves.

--- main.py
+++ main.py
@@ -4,17 +4,30 @@ from player import *
 
 def main():
     pygame.init
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     clock = pygame.time.Clock()
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
         screen.fill((0, 0, 0))
-        player.draw(screen)
+        
+        for d in drawable:
+            d.draw(screen)
+
+        for u in updatable:
+            u.update(dt)
+
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
